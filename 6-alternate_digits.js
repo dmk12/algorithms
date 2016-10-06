3417794ddb11885e3c52bd073c9cc546a07f4a54
Dina <xdinak@gmail.com>
2016-10-06 18:25:50 +0100

    Update 6-alternate_digits.js

--- 6-alternate_digits.js
+++ 6-alternate_digits.js
@@ -1,16 +1,18 @@
 (function(a,b){
-var c = 1;
-var r = 0;
+    var c = 1;
+    var r = 0;
 
-while(a!==0 && b!==0){
-var x = (a%10)*c;
-var y = (b%10)*c*10;
-  r+=(x+y);
-  a=parseInt(a/10);
-  b=parseInt(b/10);
-  c*=100;
-}
-r+=((a+b)*c)
-return r;
+    while(a!==0 && b!==0){
+    var x = (a%10)*c;
+    var y = (b%10)*c*10;
+        r+=(x+y);
+        a=parseInt(a/10);
+        b=parseInt(b/10);
+        c*=100;
+    }
+    r+=((a+b)*c)
+
+    return r;
 
 })(1234,456789);
+//returns 4561728394
