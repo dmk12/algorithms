82eae1c4f800485f6c50d3d4e9e94236500a319b
Dina <xdinak@gmail.com>
2016-10-06 18:22:44 +0100

    Update 6-alternate_digits.js

--- 6-alternate_digits.js
+++ 6-alternate_digits.js
@@ -5,12 +5,12 @@ var r = 0;
 while(a!==0 && b!==0){
 var x = (a%10)*c;
 var y = (b%10)*c*10;
-  r=r+x+y;
+  r+=(x+y);
   a=parseInt(a/10);
   b=parseInt(b/10);
   c*=100;
 }
-r = r+((a+b)*c)
+r+=((a+b)*c)
 return r;
 
 })(1234,456789);
